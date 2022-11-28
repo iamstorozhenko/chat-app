626453173236a73f077e0c847d1e7059b91f95f0
iamstorozhenko <iamantonstorozhenko@gmail.com>
2022-11-28 23:55:48 +0200

    added backend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import io from "socket.io-client";
 import "./App.css";
 
-function App() {
+const socket = io.connect("http://localhost:4444");
+
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <Home
+                username={username}
+                setUsername={setUsername}
+                room={room}
+                setRoom={setRoom}
+                socket={socket}
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,20 +1,42 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
-const Home = () => {
+const Home = ({ username, setUsername, room, setRoom, socket }) => {
+  const navigate = useNavigate();
+
+  const joinRoom = () => {
+    if (room !== "" && username !== "") {
+      socket.emit("join_room", { username, room });
+    }
+
+    navigate("/chat", { replace: true });
+  };
+
   return (
-    <div>
-      <h1>Hello mate</h1>
-      <input className="main-input" placeholder="Username..." />
+    <div className="container-wrapper">
+      <div className="card-wrapper">
+        <h1>Hello mate</h1>
+        <div className="form-wrapper">
+          <input
+            className="main-input"
+            placeholder="Username..."
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-      <select>
-        <option>Select room</option>
-        <option>Javascript</option>
-        <option>Node</option>
-        <option>React</option>
-      </select>
+          <select onChange={(e) => setRoom(e.target.value)}>
+            <option>Select room</option>
+            <option value="Sport">Sport</option>
+            <option value="Gaming">Gaming</option>
+            <option value="Politics">Politics</option>
+            <option value="Talking">Talking</option>
+          </select>
 
-      <button>Join room</button>
+          <button className="main-button" onClick={joinRoom}>
+            Join room
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
